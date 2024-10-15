9ae95be1f441928cbbb07dfc1b3e1d3005115f5c
luisdacrash <luisdacrash2@gmail.com>
2024-10-15 20:48:59 +0200

    Arreglo de /show e imports

--- ReaktorIssuesServer/src/main/java/es/iesjandula/ReaktorIssuesServer/rest/ServerIssuesRest.java
+++ ReaktorIssuesServer/src/main/java/es/iesjandula/ReaktorIssuesServer/rest/ServerIssuesRest.java
@@ -1,8 +1,10 @@
 package es.iesjandula.ReaktorIssuesServer.rest;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -53,7 +55,7 @@ public class ServerIssuesRest {
 	}
 	
 	
-	@RequestMapping(method = RequestMethod.GET, value = "/show", consumes = "multipart/form-data")
+	@RequestMapping(method = RequestMethod.GET, value = "/show")
 	public ResponseEntity<?> mostrarIncidencias()
 	{
 		try {
